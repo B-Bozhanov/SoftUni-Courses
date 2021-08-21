9776cce7be219acce8983ab51588ba4885a53878
Bozhan Bozhanov <bojanbojanovsot@gmail.com>
2021-08-21 17:24:26 +0300

    Added-tasks-from-05-to-09

---
+++ Programming Basics with C#/First Steps In Coding - Lab/T08PetShop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace T08PetShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double DOG_FOOD = 2.50;
+            const double CAT_FOOD = 4.00;
+
+            int dogPackages = int.Parse(Console.ReadLine());
+            int catPackages = int.Parse(Console.ReadLine());
+
+            double totalSum = (dogPackages * DOG_FOOD) + (catPackages * CAT_FOOD);
+
+            Console.WriteLine($"{totalSum} lv.");
+        }
+    }
+}
---
+++ Programming Basics with C#/First Steps In Coding - Lab/T09YardGreening/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace T09YardGreening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double squareMeters = double.Parse(Console.ReadLine());
+            double totalSum = (squareMeters * 7.61) * 0.82;
+            double discount = (squareMeters * 7.61) * 0.18;
+
+            Console.WriteLine($"The final price is: {totalSum:f2} lv.");
+            Console.WriteLine($"The discount is: {discount:f2} lv.");      	
+        }
+    }
+}
