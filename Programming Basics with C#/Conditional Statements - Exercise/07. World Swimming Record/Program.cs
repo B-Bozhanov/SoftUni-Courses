d03646e8d6f388244d9def9aa886373dad44537c
Bozhan Bozhanov <bojanbojanovsot@gmail.com>
2021-08-27 17:48:35 +0300

    Added-Exercises-from-Conditional-Statement

---
+++ Programming Basics with C#/Conditional Statements - Exercise/07. World Swimming Record/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _07._World_Swimming_Record
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double worldRecord = double.Parse(Console.ReadLine());
+            double distance = double.Parse(Console.ReadLine());
+            double timePerMeter = double.Parse(Console.ReadLine());
+
+            double delay = Math.Floor (distance / 15 ) * 12.5;
+            double totalTime = timePerMeter * distance + delay;
+
+            if (totalTime < worldRecord)
+            {
+                Console.WriteLine($" Yes, he succeeded! The new world record is {totalTime:f2} seconds.");
+            }
+            else
+            {
+                Console.WriteLine($"No, he failed! He was {totalTime - worldRecord:f2} seconds slower.");
+            }
+
+        }
+    }
+}
---
+++ Programming Basics with C#/Conditional Statements - Exercise/08. Lunch Break/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _08._Lunch_Break
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string movieName = Console.ReadLine();
+            double durationMovie = double.Parse(Console.ReadLine());
+            double durationRelax = double.Parse(Console.ReadLine());
+
+            double neededTimeForLunch = durationRelax / 8;
+            double neededTimeForRest = durationRelax / 4;
+            double totalTime = durationRelax - (neededTimeForLunch + neededTimeForRest);
+            double neededTime = Math.Abs(durationMovie - totalTime);
+
+            if (totalTime >= durationMovie)
+            {
+                Console.WriteLine($"You have enough time to watch {movieName} and left with {Math.Ceiling(neededTime)} minutes free time.");
+            }
+            else
+            {
+                Console.WriteLine($"You don't have enough time to watch {movieName}, you need {Math.Ceiling(neededTime)} more minutes.");
+            }
+        }
+    }
+}
