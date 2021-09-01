30edd0a191d534fd5fda047f42171ad9482005f9
Bozhan Bozhanov <bojanbojanovsot@gmail.com>
2021-09-01 18:10:21 +0300

    Add-ForLoop-from-Lab

---
+++ Programming Basics with C#/For Loop - Lab/06. Vowels Sum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _06._Vowels_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            char vowels = ' ';
+            int totalSum = 0;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                char vowel = word[i];
+                switch (vowel)
+                {
+                    case 'a':
+                        totalSum += 1;break;
+                    case 'e':
+                        totalSum += 2; break;
+                    case 'i':
+                        totalSum += 3; break;
+                    case 'o':
+                        totalSum += 4; break;
+                    case 'u':
+                        totalSum += 5; break;
+                }
+            }
+            Console.WriteLine(totalSum);
+        }
+    }
+}
