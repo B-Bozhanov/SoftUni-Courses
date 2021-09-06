0a60577e62cf8a4872891838815e796235d349e0
Bozhan Bozhanov <bojanbojanovsot@gmail.com>
2021-09-06 18:27:38 +0300

    Added-Drawing-figres-with-Loops

---
+++ Programming Basics with C#/Drawing Figures with Loops - More Exercises/05. Square Frame/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+
+                if (i == 1 || i == n)
+                {
+                    Console.Write($"+ ");
+                }
+                else
+                {
+                    Console.Write($"| ");
+                }
+
+
+
+                for (int j = 1; j <= n - 2; j++)
+                {
+                    Console.Write($"- ");
+                }
+                if (i == 1 || i == n)
+                {
+                    Console.Write($"+ ");
+                }
+                else
+                {
+                    Console.Write($"| ");
+                }
+
+                Console.WriteLine();
+
+
+
+            }
+        }
+    }
+}
---
+++ Programming Basics with C#/Drawing Figures with Loops - More Exercises/06. Rhombus of Star/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _06._Rhombus_of_Star
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+
+            // 5
+            // * == " " n-1
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = i; j <= n -1; j++)
+                {
+                    Console.Write($" ");
+                }
+
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write($"* ");
+                }
+                Console.WriteLine();
+            }
+            for (int i = n - 1; i >= 1; i--)
+            {
+                for (int j = 1; j <= n - i; j++)
+                {
+                    Console.Write($" ");
+                }
+
+                for (int j = i; j >= 1; j--)
+                {
+                    Console.Write($"* ");
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
+
