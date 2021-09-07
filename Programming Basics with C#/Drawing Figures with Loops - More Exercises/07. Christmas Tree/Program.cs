4af3b41250eede088b8fa945f6161ac0c80e3f58
Bozhan Bozhanov <bojanbojanovsot@gmail.com>
2021-09-07 18:28:47 +0300

    Added-Drawing-Figures-with-Loops

---
+++ Programming Basics with C#/Drawing Figures with Loops - More Exercises/07. Christmas Tree/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _07._Christmas_Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            NewMethod1(n);
+            Console.WriteLine($" | ");
+            for (int i = 1; i <= n; i++)
+            {
+                for (int k = i; k <= n - 1; k++)
+                {
+                    Console.Write($" ");
+                }
+                NewMethod(i);
+                Console.Write($" | ");
+                NewMethod(i);
+                Console.WriteLine();
+            }
+        }
+
+        private static void NewMethod1(int n)
+        {
+            for (int i = 0; i <= n - 1; i++)
+            {
+                Console.Write($" ");
+            }
+        }
+
+        private static void NewMethod(int i)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write($"*");
+            }
+        }
+    }
+}
---
+++ Programming Basics with C#/Drawing Figures with Loops - More Exercises/08. Sunglasses/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _08._Sunglasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = double.Parse(Console.ReadLine());
+            double middle = Math.Ceiling(n / 2);
+
+            for (int rows = 1; rows <= n; rows++)
+            {
+                for (int i = 1; i <= n * 2 * 2; i++)
+                {
+                    if (rows == 1 || rows == n)
+                    {
+                        Console.Write($"*");
+                    }
+                    else
+                    {
+                        if (i == 1 || i == n * 4 / 2 || i == n * 4 / 2 + 1 || i == n * 4)
+                        {
+                            Console.Write($"*");
+                        }
+                        else
+                        {
+                            Console.Write($"/");
+                        }                       
+                    }
+                    
+                    if (i == n * 4 / 2)
+                    {
+                        for (int distance = 0; distance < n; distance++)
+                        {
+                            if (rows == middle)
+                            {
+                                Console.Write($"|");
+                            }
+                            else
+                            {
+                                Console.Write($" ");
+                            }
+                        }
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
