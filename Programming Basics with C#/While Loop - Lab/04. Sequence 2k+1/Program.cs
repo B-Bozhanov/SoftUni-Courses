2e0ba1b3ee55a1c444b25eab3a115ca562623b34
Bozhan Bozhanov <bojanbojanovsot@gmail.com>
2021-09-09 18:36:51 +0300

    Added-WhileLoop-Lab

---
+++ Programming Basics with C#/While Loop - Lab/04. Sequence 2k+1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _04._Sequence_2k_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int counter = 1;
+
+            while (number >= counter)
+            {
+                Console.WriteLine(counter);
+                counter = counter * 2 + 1;
+            }
+            //for (int i = 1; i <= number; i++)
+            //{
+            //    Console.WriteLine(i);
+            //    i *= 2 ;
+            //}
+        }
+    }
+}
---
+++ Programming Basics with C#/While Loop - Lab/05. Account Balance/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _05._Account_Balance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            double totalMoney = 0;
+            string command = Console.ReadLine();
+
+            while (command != "NoMoreMoney")
+            {
+                double currentMoney = double.Parse(command);
+               
+                if (currentMoney < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }              
+                    totalMoney += currentMoney;
+                    Console.WriteLine($"Increase: {currentMoney:f2}");
+                
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"Total: {totalMoney:f2}");
+        }
+    }
+}
