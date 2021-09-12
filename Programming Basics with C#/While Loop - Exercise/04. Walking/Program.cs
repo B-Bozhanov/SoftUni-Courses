8f101cf585b0a54d13366986f4b50926e49c55d9
Bozhan Bozhanov <bojanbojanovsot@gmail.com>
2021-09-12 18:40:11 +0300

    Added-WhileLoop-Exercises

---
+++ Programming Basics with C#/While Loop - Exercise/04. Walking/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _04._Walking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputSteps = 10000;           
+            string command = Console.ReadLine();
+            int currentSteps = 0;
+
+            while (command != "Going home")
+            {
+                int steps = int.Parse(command);
+                currentSteps += steps;
+                if (currentSteps >= inputSteps)
+                {
+                    Console.WriteLine("Goal reached! Good job!");
+                    Console.WriteLine($"{currentSteps - inputSteps} steps over the goal!");
+                    return;
+                }
+                command = Console.ReadLine();
+            }
+            int lastSteps = int.Parse(Console.ReadLine());
+            currentSteps += lastSteps;
+            if (currentSteps < inputSteps)
+            {
+                Console.WriteLine($"{inputSteps - currentSteps} more steps to reach goal.");
+            }
+            else if (currentSteps >= inputSteps)
+            {
+                Console.WriteLine("Goal reached! Good job!");
+                Console.WriteLine($"{Math.Abs(inputSteps - currentSteps)} steps over the goal!");
+            }
+        }
+    }
+}
---
+++ Programming Basics with C#/While Loop - Exercise/05. Coins/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace _05._Coins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.75 => 175
+            double coinChange = double.Parse(Console.ReadLine());
+            double converted = coinChange * 100;
+            int cents = (int)converted;
+            // 220
+            int counter = 0;
+            while (cents != 0)
+            {
+                if (cents >= 200)
+                {
+                    cents -= 200;
+                    counter++;
+                }
+                else if (cents >= 100)
+                {
+                    cents -= 100;
+                    counter++;
+                }
+                else if (cents >= 50)
+                {
+                    cents -= 50;
+                    counter++;
+                }
+                else if (cents >= 20)
+                {
+                    cents -= 20;
+                    counter++;
+                }
+                else if (cents >= 10)
+                {
+                    cents -= 10;
+                    counter++;
+                }
+                else if (cents >= 5)
+                {
+                    cents -= 5;
+                    counter++;
+                }
+                else if (cents >= 2)
+                {
+                    cents -= 2;
+                    counter++;
+                }
+                else if (cents >= 1)
+                {
+                    cents -= 1;
+                    counter++;
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
