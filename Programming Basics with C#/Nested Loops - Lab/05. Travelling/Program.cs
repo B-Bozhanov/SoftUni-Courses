aed5b57dd6e4f7bbd6ddcb58893ab17c2105be31
Bozhan Bozhanov <bojanbojanovsot@gmail.com>
2021-09-16 18:52:57 +0300

    Added-NestedLoop-Lab

---
+++ Programming Basics with C#/Nested Loops - Lab/05. Travelling/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _05._Travelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+             //int minMoney = int.Parse(Console.ReadLine());
+            double totalSavedMoney = 0;
+       
+            while (destination != "End")
+            {
+                double minMoney = double.Parse(Console.ReadLine());
+
+                while (minMoney >= totalSavedMoney)
+                {
+                    double money = double.Parse(Console.ReadLine());
+                    totalSavedMoney += money;
+                    if (totalSavedMoney >= minMoney)
+                    {
+                        Console.WriteLine($"Going to {destination}!");
+                        break;
+                    }
+                }         
+                totalSavedMoney = 0;
+                destination = Console.ReadLine();
+            }
+
+            //while (destination != "End")
+            //{
+            //    double minMoney = double.Parse(Console.ReadLine());
+
+            //    for (int i = 0; i <= minMoney; i++)
+            //    {
+            //        double savedMoney = double.Parse(Console.ReadLine());
+            //        totalSavedMoney += savedMoney;
+            //        if (totalSavedMoney >= minMoney)
+            //        {
+            //            Console.WriteLine($"Going to {destination}!");
+            //            break;
+            //        }
+
+            //    }
+            //    totalSavedMoney = 0;
+            //    destination = Console.ReadLine();
+            //}
+        }
+    }
+}
---
+++ Programming Basics with C#/Nested Loops - Lab/06. Building/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _06._Building
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int floor = int.Parse(Console.ReadLine());
+            int numOfRooms = int.Parse(Console.ReadLine());
+
+            for (int i = floor; i > 0; i--)
+            {
+                for (int j = 0; j < numOfRooms; j++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        if (i == floor)
+                        {
+                            Console.Write($"L{i}{j} ");
+                        }
+                        else
+                        {
+                            Console.Write($"O{i}{j} ");
+                        }
+                    }
+                    else
+                    {
+                        if (i == floor)
+                        {
+                            Console.Write($"L{i}{j} ");
+                        }
+                        else
+                        {
+                            Console.Write($"A{i}{j} ");
+                        }
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
