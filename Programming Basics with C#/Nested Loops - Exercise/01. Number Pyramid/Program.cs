3f8d6a8921e9e7d90a2e5ae9af45808819bf36a9
Bozhan Bozhanov <bojanbojanovsot@gmail.com>
2021-09-18 18:55:00 +0300

    Added-NestedLoop-Exercises

---
+++ Programming Basics with C#/Nested Loops - Exercise/01. Number Pyramid/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01._Number_Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int current = 1;
+            int figure = num * current;
+           
+            for (int i = 1; i <= num; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write($"{current} " );
+                    current++;
+                    if (current > num)
+                    {
+                        return;
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
