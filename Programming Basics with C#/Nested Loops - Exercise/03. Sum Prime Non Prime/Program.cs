fd5934c6de95c2a5bd6be8758ad202e2a9ef0585
Bozhan Bozhanov <bojanbojanovsot@gmail.com>
2021-09-20 18:56:41 +0300

    Added-NestedLoop-Exercises

---
+++ Programming Basics with C#/Nested Loops - Exercise/03. Sum Prime Non Prime/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace _03._Sum_Prime_Non_Prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            int primeSum = 0;
+            int noPrimeSum = 0;
+            while (command != "stop")
+            {
+                int flag = 0;
+                int numbers = int.Parse(command);
+                if (numbers < 0)
+                {
+                    Console.WriteLine("Number is negative.");
+                    command = Console.ReadLine();
+                    continue;
+                }
+                int primeNume = numbers / 2;
+
+                for (int i = 2; i <= primeNume; i++)
+                {
+                    if (numbers % i == 0)
+                    {
+                        noPrimeSum += numbers;
+                        flag = 1;
+                        break;
+                    }
+
+                }
+                if (flag == 0)
+                {
+                    primeSum += numbers;
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"Sum of all prime numbers is: {primeSum}");
+            Console.WriteLine($"Sum of all non prime numbers is: {noPrimeSum}");
+
+            //int n, i, m = 0, flag = 0;
+            //Console.Write("Enter the Number to check Prime: ");
+            //n = int.Parse(Console.ReadLine());
+            //m = n / 2;
+            //for (i = 2; i <= m; i++)
+            //{
+            //    if (n % i == 0)
+            //    {
+            //        Console.Write("Number is not Prime.");
+            //        flag = 1;
+            //        break;
+            //    }
+            //}
+            //if (flag == 0)
+            //    Console.Write("Number is Prime.");
+        }
+    }
+}
---
+++ Programming Basics with C#/Nested Loops - Exercise/04. Train The Trainers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _04._Train_The_Trainers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int jury = int.Parse(Console.ReadLine());
+            string presentation = Console.ReadLine();
+            double totalAverage = 0;
+            int count = 0;
+
+            while (presentation != "Finish")
+            {
+                int counter = 0;
+                double totalGrades = 0;
+                for (int i = 0; i < jury; i++)
+                {
+                    double grade = double.Parse(Console.ReadLine());
+                    counter++;
+                    totalGrades += grade;
+                }
+                double average = totalGrades / counter;
+                count++;
+                totalAverage += average;
+                Console.WriteLine($"{presentation} - {average:f2}.");
+
+                presentation = Console.ReadLine();
+            }
+            double studentAverage = totalAverage / count;
+            Console.WriteLine($"Student's final assessment is {studentAverage:f2}.");
+        }
+    }
+}
