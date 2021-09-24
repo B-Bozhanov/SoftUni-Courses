047ae3b7bb9332fa568da4ff0687b34f8859d8da
Bozhan Bozhanov <bojanbojanovsot@gmail.com>
2021-09-24 19:07:38 +0300

    Added-WhileLoop-Exercises

---
+++ Programming Basics with C#/While Loop - Exercise/04. Numbers Divided by 3 Without Reminder/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _04._Numbers_Divided_by_3_Without_Reminder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 1;
+            while (count <= 100)
+            {
+                if (count % 3 == 0)
+                {
+                    Console.WriteLine(count);
+                }
+                count++;
+            }
+
+        }
+    }
+}
---
+++ Programming Basics with C#/While Loop - Exercise/09. Average Number/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _05._Average_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double sum = 0;
+            int count = 0;
+            while (count < n)
+            {
+                int num = int.Parse(Console.ReadLine());
+                sum += num;
+                count++;
+            }
+            double average = sum / count;
+            Console.WriteLine($"{average:f2}");
+        }
+    }
+}
