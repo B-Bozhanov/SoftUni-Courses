1055e74708399418009d0c05a79e76119e653682
Bozhan Bozhanov <bojanbojanovsot@gmail.com>
2021-10-01 19:22:06 +0300

    Added-Exam-Preparation

---
+++ Programming Basics with C#/Exam Preparation/Favorite Movie/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _06._Favorite_Movie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string movie = Console.ReadLine();
+            int counter = 0;
+            int totalPoints = 0;
+            string bestMovie = string.Empty;
+
+            while (movie != "STOP")
+            {
+                counter++;
+                int curentPoints = 0;
+         
+                for (int i = 0; i < movie.Length; i++)
+                {
+                    curentPoints += movie[i];
+                    if (movie[i] >= 97 && movie[i] <= 122)
+                    {
+                        curentPoints -= 2 * movie.Length;
+                    }
+                    else if (movie[i] >= 65 && movie[i] <= 90)
+                    {
+                        curentPoints -= movie.Length;
+                    }
+                }
+                if (curentPoints > totalPoints)
+                {
+                    totalPoints = curentPoints;
+                    bestMovie = movie;
+                }
+                if (counter >= 7)
+                {
+                    Console.WriteLine($"The limit is reached.");
+                    break;
+                }
+                movie = Console.ReadLine();
+            }
+            Console.WriteLine($"The best movie for you is {bestMovie} with {totalPoints} ASCII sum.");
+        }
+    }
+}
---
+++ Programming Basics with C#/Exam Preparation/Tournament of Christmas/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace _06._Tournament_of_Christmas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int daysOfTournament = int.Parse(Console.ReadLine());
+            double money = 0;
+            int totalWins = 0;
+            int totalLoses = 0;
+
+            for (int i = 1; i <= daysOfTournament; i++)
+            {
+                double curentMoney = 0;
+                int curentWins = 0;
+                int curentLooses = 0;
+
+
+                string game = Console.ReadLine();
+                while (game != "Finish")
+                {
+                    string result = Console.ReadLine();
+                    if (result == "win")
+                    {
+                        curentMoney += 20;
+                        curentWins++;
+                    }
+                    else
+                    {
+                        curentLooses++;
+                    }
+
+                    game = Console.ReadLine();
+                }
+                if (curentWins > curentLooses)
+                {
+
+                    curentMoney *= 1.10;
+                    money += curentMoney;
+                    totalWins++;
+                }
+                else
+                {
+                    totalLoses++;
+                    money += curentMoney;
+                }
+
+            }
+            if (totalWins > totalLoses)
+            {
+                money *= 1.20;
+                Console.WriteLine($"You won the tournament! Total raised money: {money:f2}");
+            }
+            else
+            {
+                Console.WriteLine($"You lost the tournament! Total raised money: {money:f2}");
+            }
+        }
+    }
+}
