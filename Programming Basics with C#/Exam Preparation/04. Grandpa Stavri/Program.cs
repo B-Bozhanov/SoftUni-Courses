f186958858c76bf85b3a43be10bbb309af7ffd96
Bozhan Bozhanov <bojanbojanovsot@gmail.com>
2021-10-05 19:27:04 +0300

    Added-Exam-Preparation

---
+++ Programming Basics with C#/Exam Preparation/04. Grandpa Stavri/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _04._Grandpa_Stavri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double totalLiters = 0;
+            double totalDegreece = 0;
+           
+            for (int i = 1; i <= n; i++)
+            {
+                double liters = double.Parse(Console.ReadLine());
+                double degreece = double.Parse(Console.ReadLine());
+                totalLiters += liters;
+                totalDegreece += liters * degreece;
+            }
+            double averageDegreece = totalDegreece / totalLiters;
+            Console.WriteLine($"Liter: {totalLiters:f2}");
+            Console.WriteLine($"Degrees: {averageDegreece:f2}");
+            if (averageDegreece < 38)
+            {
+                Console.WriteLine("Not good, you should baking!");
+            }
+            else if (averageDegreece <= 42)
+            {
+                Console.WriteLine("Super!");
+            }
+            else if (averageDegreece > 42)
+            {
+                Console.WriteLine("Dilution with distilled water!");
+            }
+        }
+    }
+}
---
+++ Programming Basics with C#/Exam Preparation/05. Best Player/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _05._Best_Player
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string player = Console.ReadLine();
+            string bestPlayer = string.Empty;
+            int totalGoals = 0;
+
+            while (player != "END")
+            {
+                int goals = int.Parse(Console.ReadLine());
+                if (goals > totalGoals)
+                {
+                    totalGoals = goals;
+                    bestPlayer = player;
+                }
+                if (goals >= 10)
+                {
+                    Console.WriteLine($"{bestPlayer} is the best player!");
+                    Console.WriteLine($"He has scored {totalGoals} goals and made a hat-trick !!!");
+                    return;
+                }
+
+                player = Console.ReadLine();
+            }
+            if (totalGoals >= 3)
+            {
+                Console.WriteLine($"{bestPlayer} is the best player!");
+                Console.WriteLine($"He has scored {totalGoals} goals and made a hat-trick !!!");
+            }
+            else 
+            {
+                Console.WriteLine($"{bestPlayer} is the best player!");
+                Console.WriteLine($"He has scored {totalGoals} goals.");
+            }
+            
+        }
+    }
+}
