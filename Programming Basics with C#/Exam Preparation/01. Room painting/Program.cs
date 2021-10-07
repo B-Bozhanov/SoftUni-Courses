0ddf98eb560449af954cebbf2e56c8df48ce47d3
Bozhan Bozhanov <bojanbojanovsot@gmail.com>
2021-10-07 19:58:07 +0300

    Added-Exam_Preparation

---
+++ Programming Basics with C#/Exam Preparation/01. Room painting/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _01._Room_painting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int paint = int.Parse(Console.ReadLine());
+            int tapets = int.Parse(Console.ReadLine());
+            double gloves = double.Parse(Console.ReadLine());
+            double brush = double.Parse(Console.ReadLine());
+
+            double totalPriceOfProducts = (paint * 21.50) + (tapets * 5.20) + (gloves * (Math.Ceiling(tapets * 0.35))) + (brush * (Math.Floor(paint * 0.48)));
+            double delivery = totalPriceOfProducts / 15;
+            Console.WriteLine($"This delivery will cost {delivery:f2} lv.");
+            
+        }
+    }
+}
---
+++ Programming Basics with C#/Exam Preparation/02. Beer And Chips/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _02._Beer_And_Chips
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fan = Console.ReadLine();
+            double budget = double.Parse(Console.ReadLine());
+            int beers = int.Parse(Console.ReadLine());
+            int chips = int.Parse(Console.ReadLine());
+            double priceOfBeer = beers * 1.20;
+            double priceOfChips = Math.Ceiling(chips * (priceOfBeer * 0.45));
+            double totalSum = priceOfBeer + priceOfChips;
+
+            if (budget >= totalSum )
+            {
+                Console.WriteLine($"{fan} bought a snack and has {budget - totalSum:f2} leva left.");
+            }
+            else
+            {
+                Console.WriteLine($"{fan} needs {totalSum - budget:f2} more leva!");
+            }
+        }
+    }
+}
