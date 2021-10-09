0a9a574dc71588a85988225016d3f481e61ce60b
Bozhan Bozhanov <bojanbojanovsot@gmail.com>
2021-10-09 19:59:59 +0300

    Added-Exam_Preparation

---
+++ Programming Basics with C#/Exam Preparation/05. Christmas Gifts/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _05._Christmas_Gifts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            double toyPrice = 5.00;
+            double sweatersPrice = 15.00;
+            int adults = 0;
+            int kids = 0;
+
+            while (command != "Christmas")
+            {
+                double ages = double.Parse(command);
+                if (ages <= 16)
+                {
+                    kids++;
+                }
+                else
+                {
+                    adults++;
+                }
+
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"Number of adults: {adults}");
+            Console.WriteLine($"Number of kids: {kids}");
+            Console.WriteLine($"Money for toys: {kids * toyPrice}");
+            Console.WriteLine($"Money for sweaters: {adults * sweatersPrice}");
+        }
+    }
+}
---
+++ Programming Basics with C#/Exam Preparation/06. Gold Mine/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _06._Gold_Mine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int locations = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= locations; i++)
+            {
+                double averageGold = 0;
+                double totalGold = 0;
+                double averageGoldPerDay = double.Parse(Console.ReadLine());
+                int days = int.Parse(Console.ReadLine());
+                for (int j = 1; j <= days; j++)
+                {
+                    double gold = double.Parse(Console.ReadLine());
+                    totalGold += gold;
+                }
+                averageGold = totalGold / days;
+                if (averageGold >= averageGoldPerDay)
+                {
+                    Console.WriteLine($"Good job! Average gold per day: {averageGold:f2}.");
+                }
+                else
+                {
+                    Console.WriteLine($"You need {averageGoldPerDay - averageGold:f2} gold.");
+                }
+            }
+
+        }
+    }
+}
