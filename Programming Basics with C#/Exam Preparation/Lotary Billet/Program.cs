a46caa4838966c3331c2ee6ea687df8bc4cea1c8
Bozhan Bozhanov <bojanbojanovsot@gmail.com>
2021-10-09 20:17:03 +0300

    Added-Exam_Preparation

---
+++ Programming Basics with C#/Exam Preparation/Lotary Billet/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Lotary_Billet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int j = 0;
+            int k = 0;
+            int l = 0;
+            int m = 0;
+            int n = 0;
+
+            int count = 0;
+            for (int i = 0; i < num; i++)
+            {
+                for (j = 65; j <= 76; j++)
+                {
+                    if (j % 2 != 0)
+                    {
+                        continue;
+                    }
+                    for (k = 102; k >= 97; k--)
+                    {
+
+                        for (l = 65; l <= 67; l++)
+                        {
+                            for (m = 1; m <= 10; m++)
+                            {
+                                for (n = 10; n >= 1; n--)
+                                {
+                                    count++;
+                                    if (count == num)
+                                    {
+                                        Console.WriteLine($"{(char)j}{(char)k}{(char)l}{m}{n} ");
+                                        return;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ Programming Basics with C#/Exam Preparation/T02Repainting/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace T02Repainting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nylon = double.Parse(Console.ReadLine());
+            double paint = double.Parse(Console.ReadLine());
+            double thinner = double.Parse(Console.ReadLine());
+            double hours = double.Parse(Console.ReadLine());
+
+            double Price = ((nylon + 2) * 1.50) + ((paint * 1.1) * 14.50) + (thinner * 5) + 0.40;            
+            double workPrice = (Price * 0.30) * hours;
+            Console.WriteLine($"Total expenses: {(Price + workPrice):f2} lv.");
+        }
+    }
+}
