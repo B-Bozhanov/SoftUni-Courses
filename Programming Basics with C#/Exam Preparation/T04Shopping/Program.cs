6688cbf8e959112874e412e31435ce8bd3344034
Bozhan Bozhanov <bojanbojanovsot@gmail.com>
2021-10-11 20:18:40 +0300

    Added-Exam_Preparation

---
+++ Programming Basics with C#/Exam Preparation/T04Shopping/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace T04Shopping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budjet = double.Parse(Console.ReadLine());
+            int cards = int.Parse(Console.ReadLine());
+            int processors = int.Parse(Console.ReadLine());
+            int ram = int.Parse(Console.ReadLine());
+            double videoCardPrice = cards * 250;
+
+            double processorsPrice = processors * (videoCardPrice * 0.35);
+            double ramPrice = ram * (videoCardPrice * 0.1);
+            double totalPrice = totalPrice = videoCardPrice + processorsPrice + ramPrice;
+
+            if (cards > processors)
+            {
+                totalPrice *=  0.85;
+            }           
+            if (totalPrice <= budjet)
+            {
+                Console.WriteLine($"You have {(budjet - totalPrice):f2} leva left!");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! You need {(totalPrice - budjet):f2} leva more!");
+            }
+        }
+    }
+}
---
+++ Programming Basics with C#/Exam Preparation/T05Coffee Machine/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace T05Coffee_Machine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string drink = Console.ReadLine();
+            string sugar = Console.ReadLine();
+            int numOfDrink = int.Parse(Console.ReadLine());
+
+            double drinkPrice = 0;
+            
+            switch (drink)
+            {
+                case "Espresso":
+                    switch (sugar)
+                    {
+                        case "Without":
+                            drinkPrice = 0.90 * 0.65 ; break;
+                        case "Normal":
+                            drinkPrice = 1.00; break;
+                        case "Extra":
+                            drinkPrice = 1.20; break;
+                    }
+                    if (numOfDrink >= 5)
+                    {
+                        drinkPrice *= 0.75;
+                    }
+                    break;
+
+                case "Cappuccino":
+                    switch (sugar)
+                    {
+                        case "Without":
+                            drinkPrice = 1.00 * 0.65; break;
+                        case "Normal":
+                            drinkPrice = 1.20; break;
+                        case "Extra":
+                            drinkPrice = 1.60; break;
+                    }
+                    break;
+
+                case "Tea":
+                    switch (sugar)
+                    {
+                        case "Without":
+                            drinkPrice = 0.50 * 0.65; break;
+                        case "Normal":
+                            drinkPrice = 0.60; break;
+                        case "Extra":
+                            drinkPrice = 0.70; break;
+                    }
+                    break;
+            }
+            double totalPrice = numOfDrink * drinkPrice;
+            if (totalPrice > 15)
+            {
+                totalPrice *= 0.80;
+            }
+            Console.WriteLine($"You bought {numOfDrink} cups of {drink} for {totalPrice:f2} lv.");
+        }
+    }
+}
