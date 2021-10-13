b5c5d8d3c8f6a6b264d5c4029db6b25e7409441c
Bozhan Bozhanov <bojanbojanovsot@gmail.com>
2021-10-13 20:20:18 +0300

    Added-Exam_Preparation

---
+++ Programming Basics with C#/Exam Preparation/T07Club/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace T07Club
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double profit = double.Parse(Console.ReadLine());
+            string cocktailName = Console.ReadLine();
+            int numCocktail = int.Parse(Console.ReadLine());
+            double cocktailPrice = 0;
+            double totalPrice = 0;
+
+            while (cocktailName != "Party!")
+            {
+                cocktailPrice = numCocktail * (cocktailName.Length);
+                if (cocktailPrice % 2 != 0)
+                {
+                    cocktailPrice *= 0.75;
+                }
+                totalPrice += cocktailPrice;
+
+                if (profit <= totalPrice)
+                {
+                    Console.WriteLine("Target acquired.");
+                    Console.WriteLine($"Club income - {totalPrice:f2} leva.");
+                    return;
+                }
+
+                cocktailName = Console.ReadLine();
+                if (cocktailName == "Party!")
+                {
+                    Console.WriteLine($"We need {(profit - totalPrice):f2} leva more.");
+                    Console.WriteLine($"Club income - {totalPrice:f2} leva.");
+                    return;
+
+                }
+                numCocktail = int.Parse(Console.ReadLine());
+            }
+
+        }
+    }
+}
---
+++ Programming Basics with C#/Exam Preparation/T09C Game Shop/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace T09C_Game_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hearthstone
+            // Fornite
+            // Overwatch
+            // Others
+
+            int numOfGames = int.Parse(Console.ReadLine());
+
+            double Hearthstone = 0;
+            double perFornite = 0;
+            double perOverwatch = 0;
+            double perOthers = 0;
+
+            for (int n = 0; n < numOfGames; n++)
+            {
+                string nameOfGame = Console.ReadLine();
+                switch (nameOfGame)
+                {
+                    case "Hearthstone":
+                        if (nameOfGame == "Hearthstone")
+                        {
+                            Hearthstone += (100.00 / numOfGames) ;
+                        }
+                        else
+                        {
+                            Hearthstone = 0;
+                        }
+                        break;
+                    case "Fornite":
+
+                        if (nameOfGame == "Fornite")
+                        {
+                            perFornite += 100.00 / numOfGames ;
+                        }
+                        else
+                        {
+                            perFornite = 0;
+                        }
+                        break;
+                    case "Overwatch":
+                        if (nameOfGame == "Overwatch")
+                        {
+                            perOverwatch += 100.00 / numOfGames ;
+                        }
+                        else
+                        {
+                            perOverwatch = 0;
+                        }
+                        break;
+                }
+                perOthers = 100.00 - (Hearthstone + perFornite + perOverwatch);
+
+            }
+            Console.WriteLine($"Hearthstone - {Hearthstone:f2}%");
+            Console.WriteLine($"Fornite - {perFornite:f2}%");
+            Console.WriteLine($"Overwatch - {perOverwatch:f2}%");
+            Console.WriteLine($"Others - {perOthers:f2}%");
+
+
+
+        }
+    }
+}
