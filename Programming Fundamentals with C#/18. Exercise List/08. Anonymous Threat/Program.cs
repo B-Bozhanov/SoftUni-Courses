29654cda419c10672672676df4d9f503971c46bd
Bozhan Bozhanov <bojanbojanovsot@gmail.com>
2021-12-19 17:00:04 +0200

    Added-Pokemon-Don't-go-and-Anonymous-Thread-in-List-Exercise

---
+++ Programming Fundamentals with C#/18. Exercise List/08. Anonymous Threat/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine()
+                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                 .ToList();
+            string command = Console.ReadLine();
+
+            while (command != "3:1")
+            {
+                string[] commandArgs = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string action = commandArgs[0];
+                int index = int.Parse(commandArgs[1]);
+                int endIndex = int.Parse(commandArgs[2]);
+                int partitions = int.Parse(commandArgs[2]);
+
+                if (action == "merge")
+                {
+                    if (index < 0 && endIndex >= 0 && endIndex < names.Count)
+                    {
+                        index = 0;
+                        MergeIndexes(names, index, endIndex);
+                    }
+                    else if (endIndex >= names.Count && index >= 0 && index < names.Count)
+                    {
+                        endIndex = names.Count - 1;
+                        MergeIndexes(names, index, endIndex);
+                    }
+                    else if (index >= 0 && index < names.Count && endIndex >= 0 && endIndex < names.Count)
+                    {
+                        MergeIndexes(names, index, endIndex);
+                    }
+                    else if (index < 0 && endIndex >= names.Count)
+                    {
+                        index = 0;
+                        endIndex = names.Count - 1;
+                        MergeIndexes(names, index, endIndex);
+                    }
+                }
+                else if (action == "divide")
+                {
+                    string curentString = names[index];
+                    List<string> curentList = new List<string>();
+                    int parts = curentString.Length / partitions;
+
+                    for (int i = 0; i < partitions; i++)
+                    {
+                        int lastElements = curentString.Length - (parts * i);
+
+                        if (i == partitions - 1)
+                        {
+                            curentList.Insert(i, curentString.Substring(parts * i, lastElements));
+                        }
+                        else
+                        {
+                            curentList.Insert(i, curentString.Substring(parts * i, parts));
+                        }
+
+                    }
+                    if (parts > 0 && parts <= 100)
+                    {
+                        names.RemoveAt(index);
+                        names.InsertRange(index, curentList);
+                    }
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(" ", names));
+        }
+
+        static void MergeIndexes(List<string> names, int index, int endIndex)
+        {
+            string mergedElements = string.Empty;
+            int counter = 0;
+            for (int i = index; i <= endIndex; i++)
+            {
+                mergedElements += names[i];
+                counter++;
+            }
+            names.RemoveRange(index, counter);
+            names.Insert(index, mergedElements);
+        }
+    }
+}
---
+++ Programming Fundamentals with C#/18. Exercise List/09. Pokemon Don't Go/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09._Pokemon_Don_t_Go
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            List<int> pokemons = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            List<int> curentElements = pokemons;
+            int SumOfRemovedElement = 0;
+            int removedElement = int.MinValue;
+            
+            while (curentElements.Count != 0)
+            {
+                int index = int.Parse(Console.ReadLine());
+
+                if (index < 0)
+                {                  
+                    removedElement = pokemons[0];
+                    curentElements[0] = curentElements[^1];
+                    ReWriteListGreaterOrLess(curentElements, removedElement);
+                }
+                else if (index >= pokemons.Count)
+                {
+                    removedElement = pokemons[^1];
+                    curentElements[^1] = curentElements[0];
+                    ReWriteListGreaterOrLess(curentElements, removedElement);
+                }
+                else
+                {
+                    removedElement = pokemons[index];
+                    curentElements.RemoveAt(index);
+                    ReWriteListGreaterOrLess(curentElements, removedElement);   
+                }
+                SumOfRemovedElement += removedElement;
+            }
+            Console.WriteLine(SumOfRemovedElement);
+        }
+        private static void ReWriteListGreaterOrLess(List<int> curentElements, int removedElement)
+        {
+            for (int i = 0; i < curentElements.Count; i++)
+            {
+                if (curentElements[i] <= removedElement)
+                {
+                    curentElements[i] += removedElement;
+                }
+                else
+                {
+                    curentElements[i] -= removedElement;
+                }
+            }
+        }
+    }
+}
