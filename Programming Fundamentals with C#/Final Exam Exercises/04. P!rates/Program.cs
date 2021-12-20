cb2d90e5c3dc7d27ac3c3537054ecd2d8ec9a44e
DareBDeviL <bojanbojanovsot@gmail.com>
2021-12-20 14:46:29 +0300

    Added -new-task-Exam-Preparation

---
+++ Programming Fundamentals with C#/Final Exam Exercises/04. P!rates/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03._P_rates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
