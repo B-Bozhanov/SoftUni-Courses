9d2f6e0db3563cb8d17d6ce9199c8aac164fbaff
Bozhan Bozhanov <bojanbojanovsot@gmail.com>
2021-12-26 19:57:13 +0200

    Added Final Exam tasks

---
+++ Programming Fundamentals with C#/Final Exam Exercises/01. Password Reset/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Password_Reset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string command = Console.ReadLine();
+
+            while (command != "Done")
+            {
+                string[] commandArgs = command.Split();
+                string action = commandArgs[0];
+
+                if (action == "TakeOdd")
+                {
+                    string currPass = string.Empty;
+                    for (int i = 0; i < input.Length; i++)
+                    {
+                        if (i % 2 != 0)
+                        {
+                            currPass += input[i];
+                        }
+                    }
+                    input = currPass;
+                    Console.WriteLine(input);
+                }
+                else if (action == "Cut")
+                {
+                    int index = int.Parse(commandArgs[1]);
+                    int length = int.Parse(commandArgs[2]);
+                    string currPass = string.Empty;
+                    for (int i = 0; i < input.Length; i++)
+                    {
+                        if (i == index)
+                        {
+                            i += length;
+                            if (i >= input.Length)
+                            {
+                                break;
+                            }
+                        }
+                        currPass += input[i];
+                    }
+                    input = currPass;
+                    Console.WriteLine(input);
+                }
+                else if (action == "Substitute")
+                {
+                    string substring = commandArgs[1];
+                    string substitute = commandArgs[2];
+                    string currPass = string.Empty;
+                    if (input.Contains(substring))
+                    {
+                        currPass = input.Replace(substring, substitute);
+                        input = currPass;
+                        Console.WriteLine(input);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nothing to replace!");
+                    }
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"Your password is: {input}");
+        }
+    }
+}
---
+++ Programming Fundamentals with C#/Final Exam Exercises/02. Fancy Barcodes/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace _02._Fancy_Barcodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"@[#]+([A-Z][A-Za-z0-9]{4,}[A-Z])@[#]+";
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                
+                MatchCollection match = Regex.Matches(input, pattern);
+                string current = string.Empty;
+                string numbers = string.Empty;
+                int Flag = 0;
+                foreach (Match item in match)
+                {
+                    current = item.Groups[1].Value;
+                }
+
+                for (int j = 0; j < current.Length; j++)
+                {
+                    if (current[j] >= 48 && current[j] <= 57)
+                    {
+                        numbers += (char)current[j];
+                        Flag = 1;
+                    }
+                }
+
+                if (match.Count == 0)
+                {
+                    Console.WriteLine("Invalid barcode");
+                    continue;
+                }
+                if (Flag == 0)
+                {
+                    Console.WriteLine($"Product group: 00");
+                }
+                else
+                {
+                    Console.WriteLine($"Product group: {numbers}");
+                }
+            }
+        }
+    }
+}
