ab3bfe615f744341607b7914eef451223dcdb343
Bozhan Bozhanov <bojanbojanovsot@gmail.com>
2022-01-20 18:43:00 +0200

    Add-List-Exercises

---
+++ Programming Fundamentals with C#/18. Exercise List/04. List Operations/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._List_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            string command = Console.ReadLine();
+            //1 23 29 18 43 21 20
+            while (command != "End")
+            {
+                string[] commandArgs = command.Split();
+                string action = commandArgs[0];
+
+
+                if (action == "Add")
+                {
+                    int num = int.Parse(commandArgs[1]);
+                    numbers.Add(num);
+                }
+                else if (action == "Insert")
+                {
+                    int number = int.Parse(commandArgs[1]);
+                    int index = int.Parse(commandArgs[2]);
+                    if (index < 0 || index >= numbers.Count)
+                    {
+                        Console.WriteLine("Invalid index");
+                        command = Console.ReadLine();
+                        continue;
+                    }
+                    numbers.Insert(index, number);
+                }
+                else if (action == "Remove")
+                {
+                    int index = int.Parse(commandArgs[1]);
+                    if (index < 0 || index >= numbers.Count)
+                    {
+                        Console.WriteLine("Invalid index");
+                        command = Console.ReadLine();
+                        continue;
+                    }
+                    numbers.RemoveAt(index);
+                }
+                else
+                {
+                    int number = int.Parse(commandArgs[2]);
+                   
+                    if (commandArgs[1] == "left")
+                    {
+                        for (int j = 0; j < number; j++)
+                        {
+                            int firstElement = numbers[0];
+                            for (int i = 0; i < numbers.Count - 1; i++)
+                            {
+                                numbers[i] = numbers[i + 1];
+                            }
+                            numbers[numbers.Count - 1] = firstElement;
+
+                        }
+                    }
+                    else if (commandArgs[1] == "right")
+                    {
+                        for (int i = 0; i < number; i++)
+                        {
+                            int lastElement = numbers[numbers.Count - 1];
+                            for (int j = numbers.Count - 1; j > 0; j--)
+                            {
+                                numbers[j] = numbers[j - 1];
+                            }
+                            numbers[0] = lastElement;
+
+                        }
+                    }
+                }
+
+
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
---
+++ Programming Fundamentals with C#/18. Exercise List/06. Cards Game/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Cards_Game
+{
+    class Program
+    {
+        public static object Select { get; private set; }
+
+        static void Main(string[] args)
+        {
+            // 10 20 30 40 50
+            // 50 40 30 30 10
+
+            List<int> firstPlayer = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> secondPlayer = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int count1 = firstPlayer.Count;
+            int count2 = secondPlayer.Count;
+            for (int i = 0; i < int.MaxValue; i++)
+            {
+
+                if (firstPlayer[0] > secondPlayer[0])
+                {
+                    firstPlayer.Add(secondPlayer[0]);
+                    firstPlayer.Add(firstPlayer[0]);
+                    firstPlayer.RemoveAt(0);
+                    secondPlayer.RemoveAt(0);
+                }
+                else if (firstPlayer[0] < secondPlayer[0])
+                {
+                    secondPlayer.Add(firstPlayer[0]);
+                    secondPlayer.Add(secondPlayer[0]);
+                    firstPlayer.RemoveAt(0);
+                    secondPlayer.RemoveAt(0);
+                }
+                else
+                {
+                    firstPlayer.RemoveAt(0);
+                    secondPlayer.RemoveAt(0);
+                }
+
+                if (firstPlayer.Count == 0)
+                {
+                    int sum = 0;
+                    for (int j = 0; j < secondPlayer.Count; j++)
+                    {
+                        sum += secondPlayer[j];
+                    }
+                    Console.WriteLine($"Second player wins! Sum: {sum}");
+                    break;
+                }
+                else if (secondPlayer.Count == 0)
+                {
+                    int sum = 0;
+                    for (int j = 0; j < firstPlayer.Count; j++)
+                    {
+                        sum += firstPlayer[j];
+                    }
+                    Console.WriteLine($"First player wins! Sum: {sum}");
+                    break;
+                }
+            }
+        }
+    }
+}
