92691d7e950476523a764744bd8263e18a050ab8
DareDeviL88 <bojanbojanovsot88@gmail.com>
2022-03-24 22:25:01 +0200

    Begin Task05 SoftUni Parking

---
+++ Programming Fundamentals with C#/Associative Arrays - Exercise/05. SoftUni Parking/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._SoftUni_Parking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> registered = new Dictionary<string, string>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input =Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string action = input[0];
+                string userName = input[1];
+
+                if (action == "register")
+                {
+                    string userId = input[2];
+                    if (!registered.ContainsKey(userName))
+                    {
+                        registered.Add(userName, userId);
+                    }
+                }
+                else if (action == "unregister")
+                {
+
+                }
+            }
+        }
+    }
+}
