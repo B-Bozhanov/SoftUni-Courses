ded05db767f2a23274bbfff3ea0008e3bf7a2cca
DareDeviL88 <bojanbojanovsot88@gmail.com>
2022-03-27 14:52:39 +0300

    Added old task-03 maked with Classes

---
+++ Programming Fundamentals with C#/Final Exam Exercises/03. Heroes of Code and Logic VII - With Classes/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Heroes_of_Code_and_Logic_VII
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Raed the input.
+            int n = int.Parse(Console.ReadLine());
+
+            List<HeroesInfo> heroesInfo = new List<HeroesInfo>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] heroesArgs = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string name = heroesArgs[0];
+                int hp = int.Parse(heroesArgs[1]);
+                int mp = int.Parse(heroesArgs[2]);
+
+                if (hp > 100)
+                {
+                    hp = 100;
+                }
+                if (mp > 200)
+                {
+                    mp = 200;
+                }
+
+                heroesInfo.Add(new HeroesInfo(name, hp, mp));
+            }
+
+            string commands = Console.ReadLine();
+            while (commands != "End")
+            {
+                string[] cmdArgs = commands
+                    .Split(" - ", StringSplitOptions.RemoveEmptyEntries);
+                string action = cmdArgs[0];
+                string name = cmdArgs[1];
+
+                HeroesInfo currHero = heroesInfo.SingleOrDefault(x => x.Name == name);
+
+                if (action == "CastSpell")
+                {
+                    int mpNeeded = int.Parse(cmdArgs[2]);
+                    string spellName = cmdArgs[3];
+
+                    currHero.CastSpell(mpNeeded, spellName);
+                }
+                else if (action == "TakeDamage")
+                {
+                    int damage = int.Parse(cmdArgs[2]);
+                    string attacker = cmdArgs[3];
+
+                    if (currHero.TakeDamage(damage, attacker))
+                    {
+                        heroesInfo.Remove(currHero);
+                    }
+                }
+                else if (action == "Recharge")
+                {
+                    int amount = int.Parse(cmdArgs[2]);
+                    currHero.Recharge(amount);
+                }
+                else if (action == "Heal")
+                {
+                    int amount = int.Parse(cmdArgs[2]);
+                    currHero.Heal(amount);
+                }
+
+
+                commands = Console.ReadLine();
+            }
+
+            foreach (var hero in heroesInfo)
+            {
+                Console.WriteLine(hero.Name);
+                Console.WriteLine($"  HP: {hero.HitPoints}");
+                Console.WriteLine($"  MP: {hero.ManaPoints}");
+            }
+        }
+    }
+}
