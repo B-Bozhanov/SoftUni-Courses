a9222aa207ab931e15edce95ce77ff3d07a2cb76
DareBDeviL <bojanbojanovsot@gmail.com>
2022-03-30 13:37:21 +0300

    Refacturing task03- Need For Speed

---
+++ Programming Fundamentals with C#/Final Exam Exercises/03. Need-fo- Speed III/Cars.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace _03._Need_for_Speed_III
+{
+    class Cars
+    {
+        public string Car { get; set; }
+        public int Milleage { get; set; }
+        public int Fuel { get; set; }
+        private int maxMileage = 100000;
+        private int maxLitters = 75;
+
+        public Cars(string car, int milleage, int fuel)
+        {
+            this.Car = car;
+            this.Milleage = milleage;
+            this.Fuel = fuel;
+        }
+
+        public bool Drive(int distanceToDrive, int neededFuel)
+        {
+            bool isMileageOver = false;
+            if (Fuel < neededFuel)                           // Check if the fuel will be enough to drive the distance.
+            {
+                Console.WriteLine("Not enough fuel to make that ride");
+                return isMileageOver;
+            }
+            Milleage += distanceToDrive;
+            Fuel -= neededFuel;
+
+            Console.WriteLine($"{Car} driven for {distanceToDrive} kilometers. {neededFuel} liters of fuel consumed.");
+            if (Milleage >= maxMileage)                    // Check if the mileages are greater or equal to 100 000, and if is true - remove current car 
+            {
+                Console.WriteLine($"Time to sell the {Car}!");
+                isMileageOver = true;
+            }
+
+            return isMileageOver;
+        }
+
+        public void Refuel(int refuel)
+        {
+            if (Fuel + refuel > maxLitters)
+            {
+                refuel = maxLitters - Fuel;            // Take how fuel is needed to fill 75 liters tank, if it's greater than 75.
+                Fuel = maxLitters;
+            }
+            else
+            {
+                Fuel += refuel;
+            }
+            Console.WriteLine($"{Car} refueled with {refuel} liters");
+        }
+
+        public void Revert(int mileageToDecreese)
+        {
+            Milleage -= mileageToDecreese;
+            if (Milleage < 10000)
+            {
+                Milleage = 10000;                     // Rewrite mileages to 10 000 if it's  less than after Revert command.
+            }
+                Console.WriteLine($"{Car} mileage decreased by {mileageToDecreese} kilometers");
+        }
+    }
+}
--- Programming Fundamentals with C#/Final Exam Exercises/03. Need-fo- Speed III/Program.cs
+++ Programming Fundamentals with C#/Final Exam Exercises/03. Need-fo- Speed III/Program.cs
@@ -9,93 +9,57 @@ namespace _03._Need_for_Speed_III
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
-            Dictionary<string, int[]> carsInfo = new Dictionary<string, int[]>();
+            List<Cars> cars = new List<Cars>();
 
             for (int i = 0; i < n; i++)
             {
-                string[] input = Console.ReadLine().Split("|");
-                string car = input[0];
-                int mileage = int.Parse(input[1]);
-                int fuel = int.Parse(input[2]);
-                if (!carsInfo.ContainsKey(car))
-                {
-                    carsInfo.Add(car, new int[2] { mileage, fuel });         // Add input info to  collection.
-                }
+                string[] carInfo = Console.ReadLine()
+                    .Split("|", StringSplitOptions.RemoveEmptyEntries);
+                string car = carInfo[0];
+                int mileage = int.Parse(carInfo[1]);
+                int fuel = int.Parse(carInfo[2]);
+
+                cars.Add(new Cars(car, mileage, fuel));
             }
 
             string command = Console.ReadLine();
             while (command != "Stop")
             {
-                string[] commandArgs = command.Split(" : ");
+                string[] commandArgs = command
+                    .Split(" : ", StringSplitOptions.RemoveEmptyEntries);
                 string action = commandArgs[0];
                 string car = commandArgs[1];
 
-                int carMileage = carsInfo[car][0];                       // Get mileage and start fuel from collection.
-                int startFuel = carsInfo[car][1];
+                Cars currCar = cars.SingleOrDefault(x => x.Car == car);
 
                 if (action == "Drive")
                 {
                     int distanceToDrive = int.Parse(commandArgs[2]);
                     int neededFuel = int.Parse(commandArgs[3]);
 
-                    if (startFuel < neededFuel)                           // Check if the fuel will be enough to drive the distance.
+                    if (currCar.Drive(distanceToDrive, neededFuel))
                     {
-                        Console.WriteLine("Not enough fuel to make that ride");
-                        command = Console.ReadLine();
-                        continue;
-                    }
-                    else
-                    {
-                        carMileage += distanceToDrive;                   // Increase car mileage with the given distance
-                        startFuel -= neededFuel;                         // Decrease car fuel with the given fuel.
-                        carsInfo[car][0] = carMileage;                  // Rewrite new values in collection
-                        carsInfo[car][1] = startFuel;
-
-                        Console.WriteLine($"{car} driven for {distanceToDrive} kilometers. {neededFuel} liters of fuel consumed.");
-                        if (carMileage >= 100000)                    // Check if the mileages are greater or equal to 100 000, and if is true - remove current car 
-                                                                     // from collection.
-                        {
-                            Console.WriteLine($"Time to sell the {car}!");
-                            carsInfo.Remove(car);
-                        }
+                        cars.Remove(currCar);
                     }
                 }
                 else if (action == "Refuel")
                 {
-                    int refuel = int.Parse(commandArgs[2]);            
-                    if (startFuel + refuel > 75)
-                    {
-                        refuel = 75 - startFuel;            // Take how fuel is needed to fill 75 liters tank, if it's greater than 75.
-                        startFuel = 75;
-                    }
-                    else
-                    {
-                        startFuel += refuel;
-                    }
-                    carsInfo[car][1] = startFuel;
-                    Console.WriteLine($"{car} refueled with {refuel} liters");
+                    int refuel = int.Parse(commandArgs[2]);
+                    currCar.Refuel(refuel);
                 }
                 else if (action == "Revert")
                 {
                     int mileageToDecreese = int.Parse(commandArgs[2]);
-                    carMileage -= mileageToDecreese;
-                    if (carMileage < 10000)
-                    {
-                        carMileage = 10000;                     // Rewrite mileages to 10 000 if it's  less than after Revert command.
-                        carsInfo[car][0] = carMileage;
-                    }
-                    else
-                    {
-                        carsInfo[car][0] = carMileage;
-                        Console.WriteLine($"{car} mileage decreased by {mileageToDecreese} kilometers");
-                    }
+                    currCar.Revert(mileageToDecreese);
                 }
 
                 command = Console.ReadLine();
             }
-            foreach (var item in carsInfo.OrderByDescending(x => x.Value[0]).ThenBy(x => x.Key))  // Print collection.
+
+            foreach (Cars car in cars.OrderByDescending(x => x.Milleage).ThenBy(x => x.Car))
             {
-                Console.WriteLine($"{item.Key} -> Mileage: {item.Value[0]} kms, Fuel in the tank: {item.Value[1]} lt.");
+                Console.Write($"{car.Car} -> Mileage: {car.Milleage} km, Fuel in the tank: {car.Fuel} lt.");
+                Console.WriteLine();
             }
         }
     }
