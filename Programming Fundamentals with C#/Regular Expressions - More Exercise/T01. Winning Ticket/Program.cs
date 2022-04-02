5a6845d471267ca01c741e9dae37dd25a6b903d4
DareDeviL88 <bojanbojanovsot88@gmail.com>
2022-04-02 16:16:35 +0300

    Added Regular Expressions - More Exercise

---
+++ Programming Fundamentals with C#/Regular Expressions - More Exercise/T01. Winning Ticket/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace T01._Winning_Ticket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"([@#\$\^])\1{5,9}";
+            string[] tickets = Console.ReadLine()
+                 .Split(",", StringSplitOptions.RemoveEmptyEntries)
+                 .Select(x => x.Trim())
+                 .ToArray();
+
+            for (int i = 0; i < tickets.Length; i++)
+            {
+                string currTicket = tickets[i];
+                if (currTicket.Length != 20)
+                {
+                    Console.WriteLine("invalid ticket");
+                    continue;
+                }
+                string left = currTicket.Substring(0, currTicket.Length / 2);
+                string right = currTicket.Substring(currTicket.Length / 2);
+                Match matchLeft = Regex.Match(left, pattern);
+                Match matchRight = Regex.Match(right, pattern);
+
+                if (matchLeft.Success && matchRight.Success)
+                {
+                    int minLeft = matchLeft.Value.Length;
+                    int minRight = matchRight.Value.Length;
+                    int result = Math.Min(minLeft, minRight);
+                   
+                    if (result == 10)
+                    {
+                        Console.WriteLine($"ticket \"{currTicket}\" - {result}{matchLeft.Groups[1].Value} Jackpot!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ticket \"{currTicket}\" - {result}{matchLeft.Groups[1].Value}");
+                    }
+                    continue;
+                }
+                Console.WriteLine($"ticket \"{currTicket}\" - no match");
+            }
+        }
+    }
+}
