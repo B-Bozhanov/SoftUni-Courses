7ba6155b8ba5e5c2b5ada3ce1a3d41b9e836bbb7
DareDeviL88 <bojanbojanovsot@gmail.com>
2022-04-03 18:04:20 +0300

    T02-BasicQueueOperations

---
+++ C# Advanced/Stacks and Queues - Exercise/T02-BasicQueueOperations/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T02_BasicQueueOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => int.Parse(x))
+                .ToArray();
+
+            int n = input[0];
+            int s = input[1];
+            int x = input[2];
+
+            int[] inputNumbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => int.Parse(x))
+                .ToArray();
+
+            Queue<int> numbers = new Queue<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers.Enqueue(inputNumbers[i]);
+            }
+
+            for (int i = 0; i < s; i++)
+            {
+                numbers.Dequeue();
+            }
+
+            if (numbers.Count <= 0)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            if (numbers.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(numbers.Min());
+            }
+
+        }
+    }
+}
