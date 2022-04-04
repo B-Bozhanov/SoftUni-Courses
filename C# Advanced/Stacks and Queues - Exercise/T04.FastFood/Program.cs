f7e6981c40b1970afc39d83e7e07bf825abf8137
DareDeviL88 <bojanbojanovsot@gmail.com>
2022-04-04 20:09:50 +0300

    Task04 int StackAndQueue

---
+++ C# Advanced/Stacks and Queues - Exercise/T04.FastFood/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T04.FastFood
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalFood = int.Parse(Console.ReadLine());
+            Queue<int> orders = new Queue<int>
+                (
+                Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => int.Parse(x))
+                );
+            int maxOrder = orders.Max();
+            Console.WriteLine(maxOrder);
+
+            while (orders.Count != 0)
+            {
+                int currOrder = orders.Peek();
+                if (totalFood < currOrder)
+                {
+                    Console.WriteLine($"Orders left: {string.Join(' ', orders)}");
+                    return;
+                }
+
+                totalFood -= orders.Dequeue();
+            }
+            Console.WriteLine("Orders complete");
+        }
+    }
+}
