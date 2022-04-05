71f505f5d035def7c055e77143db7c848044b0d5
DareDeviL88 <bojanbojanovsot@gmail.com>
2022-04-05 20:16:30 +0300

    Added task 05 in Stacks and Queues - Exercise

---
+++ C# Advanced/Stacks and Queues - Exercise/T05. FashionBoutique/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T05._FashionBoutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> clothes = new Stack<int>(
+                Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => int.Parse(x)));
+            int capacity = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int boxes = 0;
+
+            if (clothes.Count == 1 && clothes.Peek() == 0 || capacity == 0)
+            {
+                //Console.WriteLine("Incorect input");
+                return;
+            }
+
+            while (clothes.Count != 0)
+            {
+                int current = clothes.Pop();
+                sum += current;
+
+                if (sum >= capacity)
+                {
+                    if (sum > capacity)
+                    {
+                        clothes.Push(current);
+                    }
+                    boxes++;
+                    sum = 0;
+                }
+                else if (clothes.Count == 0)
+                {
+                    boxes++;
+                }
+            }
+            Console.WriteLine(boxes);
+        }
+    }
+}
