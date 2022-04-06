2b851f0d92df6fbb4ffe6c26547969d552ceb966
DareDeviL88 <bojanbojanovsot@gmail.com>
2022-04-06 18:58:36 +0300

    Added T06.SongsQueue

---
+++ C# Advanced/Stacks and Queues - Exercise/T06.SongsQueue/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace T06.SongsQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> songs = new Queue<string>
+                (
+                 Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                );
+
+            string commands = Console.ReadLine();
+            while (songs.Count != 0)
+            {
+                string[] cmdArgs = commands.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string command = cmdArgs[0];
+
+                if (command == "Play")
+                {
+                    songs.Dequeue();
+                }
+                else if (command == "Add")
+                {
+                    string currentSong = commands.Substring(command.Length + 1);
+
+                    if (!songs.Contains(currentSong))
+                    {
+                        songs.Enqueue(currentSong);
+                        commands = Console.ReadLine();
+                        continue;
+                    }
+                    Console.WriteLine($"{currentSong} is already contained!");
+                }
+                else if (command == "Show")
+                {
+                    Console.WriteLine(String.Join(", ", songs));
+                }
+                
+                commands = Console.ReadLine();
+            }
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
