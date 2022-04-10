ad03ddfc10aadfb3d9094bc03ba404e2656cdb00
DareDeviL88 <bojanbojanovsot@gmail.com>
2022-04-10 20:48:33 +0300

    Added Sets and Dictionaries Advanced - Exercises

---
+++ C# Advanced/Sets and Dictionaries Advanced - Exercise/01. Unique Usernames/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Unique_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<string> userNames = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                userNames.Add(input);
+            }
+
+            foreach (var user in userNames)
+            {
+                Console.WriteLine(user);
+            }
+        }
+    }
+}
---
+++ C# Advanced/Sets and Dictionaries Advanced - Exercise/T02. SetsOfElements/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T02._SetsOfElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int n = input[0];
+            int m = input[1];
+
+            HashSet<int> firstSeq = new HashSet<int>();
+            HashSet<int> seccondSeq = new HashSet<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int inputNum = int.Parse(Console.ReadLine());
+                firstSeq.Add(inputNum);
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                int inputNum = int.Parse(Console.ReadLine());
+                seccondSeq.Add(inputNum);
+            }
+            // May with List<int> ??? = firstSeq.Intersect(seccondSeq);
+            foreach (var first in firstSeq)
+            {
+                foreach (var seccond in seccondSeq) // May with seccond.Contains(first)
+                {
+                    if (first == seccond)
+                    {
+                        Console.Write(first + " ");
+                    }
+                }
+            }
+        }
+    }
+}
