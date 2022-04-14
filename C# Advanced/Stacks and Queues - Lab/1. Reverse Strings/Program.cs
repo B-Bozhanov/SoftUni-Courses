127ee2508096684beced58ffbbca6630d5b46910
DareBDeviL <bojanbojanovsot@gmail.com>
2022-04-14 10:24:20 +0300

    Added T01 from Stacks and Queues - Lab

---
+++ C# Advanced/Stacks and Queues - Lab/1. Reverse Strings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> stack = new Stack<char>(input);
+
+            //foreach (var ch in stack)
+            //{
+            //    Console.Write(ch);
+            //}
+            while (stack.Any())
+            {
+                Console.Write(stack.Pop());
+            }
+        }
+    }
+}
