61337d892d09cdf699298046e4fb126c8f4ef2c5
DareDeviL88 <bojanbojanovsot@gmail.com>
2022-04-17 19:41:25 +0300

    Added StacksAndQueues tasks from - Lab

---
+++ C# Advanced/Stacks and Queues - Lab/4. Matching Brackets/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4._Matching_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 + (2 - (2 + 3) * 4 / (3 + 1)) * 5
+            string input = Console.ReadLine();
+            Stack<int> brackets = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    brackets.Push(i);
+                }
+                else if (input[i] == ')')
+                {
+                    int lastOpenBracket = brackets.Pop();
+                    string currentResult = input.Substring(lastOpenBracket, i + 1 - lastOpenBracket); // Also may be with For loop (from lastOpenBracket to i!)
+                    Console.WriteLine(currentResult);
+                }
+            }
+        }
+    }
+}
