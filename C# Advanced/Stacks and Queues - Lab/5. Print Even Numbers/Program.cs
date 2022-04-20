415f127756352134d3d311cb02f4769a4852a18c
DareDeviL88 <bojanbojanovsot@gmail.com>
2022-04-20 19:48:33 +0300

    Finaly Added T05 from StucksAndQueues Lab

--- C# Advanced/Stacks and Queues - Lab/5. Print Even Numbers/Program.cs
+++ C# Advanced/Stacks and Queues - Lab/5. Print Even Numbers/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _5._Print_Even_Numbers
 {
@@ -6,7 +8,31 @@ namespace _5._Print_Even_Numbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Queue<int> queue = new Queue<int>(input);
+
+            int lastElement = queue.LastOrDefault();
+
+            while (queue.Any(x => x % 2 != 0))
+            {
+                int currNum = queue.Dequeue();
+
+                if (currNum % 2 == 0)
+                {
+                    queue.Enqueue(currNum);
+                }
+                if (queue.All(x => x % 2 == 0))
+                {
+                    if (lastElement % 2 == 0)
+                    {
+                        queue.Enqueue(queue.Dequeue());
+                    }
+                }
+            }
+            Console.WriteLine(String.Join(", ", queue));
         }
     }
 }
+// 1 2 3 4 5 6
+// 1 2 3 4 5 5
+// 11 13 18 95 2 112 81 46
