98841de4595b09cc0f735dc9e67ee1471217062c
DareDeviL88 <bojanbojanovsot@gmail.com>
2022-04-22 12:17:39 +0300

    Complete Lab From QueuesAndStacks

---
+++ C# Advanced/Stacks and Queues - Lab/8. Traffic Jam/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _8._Traffic_Jam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int carsPassed = int.Parse(Console.ReadLine());
+            Queue<string> carList = new Queue<string>();
+            int count = 0;
+
+            while (true)
+            {
+                string cars = Console.ReadLine();
+
+                if (cars == "end")
+                {
+                    break;
+                }
+                if (cars == "green")
+                {
+                    if (carList.Count < carsPassed)
+                    {
+                        carsPassed = carList.Count;
+                    }
+                    for (int i = 0; i < carsPassed; i++)
+                    {
+                        Console.WriteLine($"{carList.Dequeue()} passed!");
+                        count++;
+                    }
+                    continue;
+                }
+                carList.Enqueue(cars);
+            }
+
+            Console.WriteLine($"{count} cars passed the crossroads.");
+        }
+    }
+}
---
+++ C# Advanced/Stacks and Queues - Lab/T06. Supermarket/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T06._Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Queue<string> clients = new Queue<string>();
+
+            while (input != "End")
+            {
+                if (input != "Paid")
+                {
+                    clients.Enqueue(input);
+
+                }
+                if (input == "Paid")
+                {
+                    foreach (var item in clients)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    clients.Clear();
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"{clients.Count} people remaining.") ;
+        }
+    }
+}
