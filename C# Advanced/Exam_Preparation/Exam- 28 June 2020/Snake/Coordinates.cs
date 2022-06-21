bd39d22f80b171cd3ff1ba4b342cb2d774c5f709
DareBDeviL <bojanbojanovsot@gmail.com>
2022-06-21 15:26:39 +0300

    New task

---
+++ C# Advanced/Exam_Preparation/Exam- 28 June 2020/Snake/Coordinates.cs
@@ -0,0 +1,14 @@
+﻿namespace Snake
+{
+    public class Coordinates
+    {
+        public Coordinates(int row, int col)
+        {
+            Row = row;
+            Col = col;
+        }
+
+        public int Row { get; set; }
+        public int Col { get; set; }
+    }
+}
