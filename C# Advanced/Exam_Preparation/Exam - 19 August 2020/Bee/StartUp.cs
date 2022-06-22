4477e24973024c7ea638a50d4c3f73c8e4e3c9c6
DareBDeviL <bojanbojanovsot@gmail.com>
2022-06-22 16:04:48 +0300

    New tasks

---
+++ C# Advanced/Exam_Preparation/Exam - 19 August 2020/Bee/StartUp.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace Bee
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var fieldSize = int.Parse(Console.ReadLine());
+            var field = new char[fieldSize, fieldSize];
+            FillMatrix(field);
+            int[] beeCords = GetBeeCordinates(field);
+            var beeRow = beeCords[0];
+            var beeCol = beeCords[1];
+            var pollinatedFlowers = 0;
+
+            var command = Console.ReadLine();
+            while (command != "End")
+            {
+                var directRow = 0;
+                var directCol = 0;
+
+                switch (command)
+                {
+                    case "up": directRow -= 1; break;
+                    case "down": directRow += 1; break;
+                    case "left": directCol -= 1; break;
+                    case "right": directCol += 1; break;
+                }
+
+                field[beeRow, beeCol] = '.';
+                beeRow += directRow;
+                beeCol += directCol;
+
+                if (!IsRange(field, beeRow, beeCol))
+                {
+                    Console.WriteLine("The bee got lost!");
+                    break;
+                }
+                else if (field[beeRow, beeCol] == 'O')
+                {
+                    field[beeRow, beeCol] = '.';
+                    continue;
+                }
+                if (field[beeRow, beeCol] == 'f')
+                {
+                    pollinatedFlowers++;
+                }
+
+                field[beeRow, beeCol] = 'B';
+
+                command = Console.ReadLine();
+            }
+
+            if (pollinatedFlowers >= 5)
+            {
+                Console.WriteLine($"Great job, the bee managed to pollinate {pollinatedFlowers} flowers!");
+            }
+            else
+            {
+                Console.WriteLine($"The bee couldn't pollinate the flowers, she needed {5 - pollinatedFlowers} flowers more");
+            }
+            PrintMatrix(field);
+        }
+
+        private static void PrintMatrix(char[,] field)
+        {
+            for (int row = 0; row < field.GetLength(0); row++)
+            {
+                for (int col = 0; col < field.GetLength(1); col++)
+                {
+                    Console.Write(field[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+        private static int[] GetBeeCordinates(char[,] field)
+        {
+            var beeDirection = new int[2];
+            for (int row = 0; row < field.GetLength(0); row++)
+            {
+                for (int col = 0; col < field.GetLength(1); col++)
+                {
+                    if (field[row, col] == 'B')
+                    {
+                        beeDirection[0] = row;
+                        beeDirection[1] = col;
+                    }
+                }
+            }
+            return beeDirection;
+        }
+
+        private static void FillMatrix(char[,] field)
+        {
+            for (int row = 0; row < field.GetLength(0); row++)
+            {
+                var input = Console.ReadLine();
+                for (int col = 0; col < field.GetLength(1); col++)
+                {
+                    field[row, col] = input[col];
+                }
+            }
+        }
+
+        private static bool IsRange(char[,] field, int row, int col)
+        {
+            return row >= 0 && row < field.GetLength(0)
+                && col >= 0 && col < field.GetLength(1);
+        }
+    }
+}
---
+++ C# Advanced/Exam_Preparation/Exam - 19 August 2020/Flower Wreaths/StartUp.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Flower_Wreaths
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> lilies = new Stack<int>(
+                Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse).ToArray());
+
+            Queue<int> roses = new Queue<int>(
+                Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse).ToArray());
+
+            int wreaths = 0;
+            int storedFlowers = 0;
+
+            while (lilies.Any() && roses.Any())
+            {
+                var currentLilies = lilies.Peek();
+                var currentRoses = roses.Peek();
+
+                while (currentLilies + currentRoses > 15)
+                {
+                    currentLilies -= 2;
+                }
+
+                if (currentLilies + currentRoses < 15)
+                {
+                    storedFlowers += currentLilies + currentRoses;
+                    lilies.Pop();
+                    roses.Dequeue();
+                    continue;
+                }
+                wreaths++;
+                lilies.Pop();
+                roses.Dequeue();
+            }
+
+            while (storedFlowers >= 15)
+            {
+                wreaths++;
+                storedFlowers -= 15;
+            }
+
+            if (wreaths >= 5)
+            {
+                Console.WriteLine($"You made it, you are going to the competition with {wreaths} wreaths!");
+            }
+            else
+            {
+                Console.WriteLine($"You didn't make it, you need {5 - wreaths} wreaths more!");
+            }
+        }
+    }
+}
