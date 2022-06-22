c619823f9e28ce271f7595b16f64ecc60d95ba9c
DareBDeviL <bojanbojanovsot@gmail.com>
2022-06-22 16:17:43 +0300

    Added new class Pet in VetClinic task

---
+++ C# Advanced/Exam_Preparation/Exam - 19 August 2020/VetClinic/Pet.cs
@@ -0,0 +1,21 @@
+﻿namespace VetClinic
+{
+    public class Pet
+    {
+        public Pet(string name, int age, string owner)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Owner = owner;
+        }
+
+        public string Name { get; }
+        public int Age { get; }
+        public string Owner { get; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name} Age: {Age} Owner: {Owner}";
+        }
+    }
+}
