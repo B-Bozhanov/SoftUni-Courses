18a2de17db1416456928daec52ed517394284129
DareDeviL88 <bojanbojanovsot@gmail.com>
2022-06-26 14:30:15 +0300

    new task

---
+++ C# OOP/Inheritance - Exercises/Restaurant/Coffe.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    internal class Coffee : HotBeverage
+    {
+        private const double CoffieMilliliters = 50;
+        private const decimal CoffeePrice = 3.50m;
+        public Coffee(string name, double caffeine) 
+            : base(name, CoffeePrice, CoffieMilliliters)
+        {
+            this.Caffeine = caffeine;
+        }
+
+        public double Caffeine { get; set; }
+    }
+}
---
+++ C# OOP/Inheritance - Exercises/Restaurant/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace Restaurant
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Cake cake = new Cake("Choco");
+            System.Console.WriteLine(cake);
+        }
+    }
+}
