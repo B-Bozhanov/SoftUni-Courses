2155fc3ef32257349ac0357ff9fd510a783a4da0
DareDeviL88 <bojanbojanovsot@gmail.com>
2022-06-26 16:24:12 +0300

    some changes

--- C# OOP/Inheritance - Exercises/Restaurant/Product.cs
+++ C# OOP/Inheritance - Exercises/Restaurant/Product.cs
@@ -10,11 +10,5 @@
 
         public string Name { get; set; }
         public decimal Price { get; set; }
-
-
-        public override string ToString()
-        {
-            return $"{Name} --> {Price}";
-        }
     }
 }
--- C# OOP/Inheritance - Exercises/Restaurant/StartUp.cs
+++ C# OOP/Inheritance - Exercises/Restaurant/StartUp.cs
@@ -4,8 +4,6 @@
     {
         public static void Main(string[] args)
         {
-            Cake cake = new Cake("Choco");
-            System.Console.WriteLine(cake);
         }
     }
 }
