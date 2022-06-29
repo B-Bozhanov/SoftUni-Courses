e7a3754ee6b8eacee975923e1a529324e05da055
DareDeviL88 <bojanbojanovsot@gmail.com>
2022-06-29 17:20:14 +0300

    new task

---
+++ C# OOP/Encapsulation - Exercises/ClassBoxData/Box.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace T01ClassBoxData
+{
+    class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public double Length
+        {
+            get { return this.length; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+
+                this.length = value;
+            }
+        }
+        public double Width
+        {
+            get { return this.width; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+
+                this.width = value;
+            }
+        }
+        public double Height
+        {
+            get { return this.height; }
+            set
+            {
+                if (value <= 0)
+                {
+                   throw new ArgumentException("Height cannot be zero or negative.");
+                }
+
+                this.height = value;
+            }
+        }
+
+        public double LateralSurfaceArea()
+        {
+            return 2 * Length * Height + 2 * Width * Height;
+        }
+        public double SurfaceArea()
+        {
+            return 2 * Length * Height + 2 * Width * Height + 2 * length * Width;
+        }
+        public double Volume()
+        {
+            return Length * Width * Height;
+        }
+    }
+}
---
+++ C# OOP/Encapsulation - Exercises/ClassBoxData/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace T01ClassBoxData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double hight = double.Parse(Console.ReadLine());
+
+            Box box;
+
+            try
+            {
+               box = new Box(length, width, hight);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            Console.WriteLine($"Surface Area - {box.SurfaceArea():f2}");
+            Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():f2}");
+            Console.WriteLine($"Volume - {box.Volume():f2}");
+        }
+    }
+}
