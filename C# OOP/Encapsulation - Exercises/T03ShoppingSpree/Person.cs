87eb7b17fe5f88f25386df002229c23d417c7716
DareBDeviL <bojanbojanovsot@gmail.com>
2022-07-04 09:01:45 +0300

    Merge

---
+++ C# OOP/Encapsulation - Exercises/T03ShoppingSpree/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace T03ShoppingSpree
+{
+    class Person
+    {
+        private string name;
+        private decimal money;
+        public Person(string name, decimal money)
+        {
+            Name = name;
+            Money = money;
+            this.Bag = new List<string>();
+        }
+
+        internal string Name
+        {
+            get => this.name;
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Name cannot be empty");
+                }
+
+                this.name = value;
+            }
+        }
+        internal decimal Money
+        {
+            get => this.money;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("Money cannot be negative");
+                }
+                this.money = value;
+            }
+        }
+        internal string PersonInfo { get => GetPersonInfo(); }
+        internal List<string> Bag { get; private set; }
+
+       private string GetPersonInfo()
+        {
+            return $"{this.Name} - {string.Join(", ", Bag)}";
+        }
+    }
+}
---
+++ C# OOP/Encapsulation - Exercises/T03ShoppingSpree/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace T03ShoppingSpree
+{
+    internal class Product
+    {
+        private string name;
+        private decimal cost;
+        public Product(string name, decimal cost)
+        {
+            Name = name;
+            Cost = cost;
+        }
+
+        public string Name
+        {
+            get => this.name;
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Name cannot be empty");
+                }
+
+                this.name = value;
+            }
+        }
+        public decimal Cost
+        {
+            get => this.cost;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("Money cannot be negative");
+                }
+                this.cost = value;
+            }
+        }
+    }
+}
