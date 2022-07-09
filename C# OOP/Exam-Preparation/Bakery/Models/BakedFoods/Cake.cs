4ed4ddb2bedb710b367051e26270b5fef032c09e
DareDeviL88 <bojanbojanovsot@gmail.com>
2022-07-09 20:07:47 +0300

    new tasks

---
+++ C# OOP/Exam-Preparation/Bakery/Models/BakedFoods/Cake.cs
@@ -0,0 +1,12 @@
+﻿namespace Bakery.Models.BakedFoods
+{
+    public class Cake : BakedFood
+    {
+        private const int DefaultCakePortion = 245;
+
+        public Cake(string name, decimal price) 
+            : base(name, DefaultCakePortion, price)
+        {
+        }
+    }
+}
